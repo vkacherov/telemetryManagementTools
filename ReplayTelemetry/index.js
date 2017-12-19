15b18685006bfec1216ceacd40c59591d5bf15fd
Vadim Kacherov <vkacherov@gmail.com>
2017-12-19 16:24:44 -0500

    +JSON processing

--- ReplayTelemetry/index.js
+++ ReplayTelemetry/index.js
@@ -1,8 +1,16 @@
 module.exports = function(context, req, jsonBlob) {
     context.log("Retrieved records:", jsonBlob);
+
+    let result = "";
+    // Parse the blob JSON
+    JSON.parse(jsonBlob, (key, value) => {
+        result[key] = value;
+        context.log("Adding key:" + key + ", value:" + value);
+    });
+
     context.res = {
         status: 200,
-        body: jsonBlob
+        body: result
     };
     context.done();
 };
